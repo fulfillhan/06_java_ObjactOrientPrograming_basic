b8df7a22a62727ac4cbd12a9f5bcb067737dd083
15_web_hsh <15_web_hsh@DESKTOP-HHECRK8>
2024-01-04 17:48:51 +0900

    2024-01-04 업데이트

--- 06_java_ObjactOrientProgaming_basic/src/step6_01/classObject/ClassEx07_연습.java
+++ 06_java_ObjactOrientProgaming_basic/src/step6_01/classObject/ClassEx07_연습.java
@@ -3,6 +3,7 @@ package step6_01.classObject;
 
  /* # 영화관 좌석예매 : 클래스 + 
 
+
  * 1. 사용자로부터 좌석번호(index)를 입력받아 예매하는 시스템이다.
  * 2. 예매가 완료되면 해당 좌석 값을 1로 변경한다.
  * 3. 이미 예매가 완료된 좌석은 재구매할 수 없다.
@@ -46,10 +47,11 @@ public class ClassEx07_연습 {
 		while (true) {
 			for (int i = 0; i < e.seat.length; i++) {
 				if(e.seat[i] == 0) {
-					System.out.println("[X] ");
+					System.out.print("[X] ");
 				}
-				else System.out.println("[O] ");
+				else System.out.print("[O] ");
 			}
+			System.out.println();
 			
 			System.out.println("[MEGA MOVIE]");
 			System.out.println("[1]좌석예매");
@@ -69,7 +71,16 @@ public class ClassEx07_연습 {
 				else System.out.println("이미 예매가 완료된 자리입니다!");
 			}
 			else if (sel == 2) {
-				//여기서부터 
+//				4. 한 좌석당 예매 가격은 12000원이다.
+//			    5. 프로그램 종료 후, 해당 영화관의 총 매출액을 출력한다.
+				/* ----------------------
+				 * 매출액 : 24000원
+				 */
+				int bookCnt = e.bookCnt;
+				bookCnt = bookCnt * 12000;
+				System.out.println("------------------");
+				System.out.println("총 매출액 : "+ bookCnt);
+				System.out.println("종료");
 				break;
 			}
 		}
